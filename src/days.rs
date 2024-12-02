53a9c0e213f81b536bc417e08120d1bd2b1f895d
sirh3e <sirh3e@protonmail.com>
2024-12-02 21:05:56 +0100

    refactored part1 to be more functional

--- src/days.rs
+++ src/days.rs
@@ -3,6 +3,7 @@ use crate::{
     Day, DayResult, ResultValue,
 };
 use std::collections::HashMap;
+use std::iter::zip;
 
 pub struct Day01;
 impl Day for Day01 {
@@ -54,41 +55,69 @@ impl Day for Day02 {
     }
 
     fn part1(&self) -> DayResult {
-        #[derive(Eq, PartialEq, Copy, Clone)]
+        #[derive(Eq, PartialEq, Copy, Clone, Debug)]
         enum Direction {
-            Up,
-            Down,
+            Up(u32),
+            Down(u32),
+        }
+
+        #[derive(Eq, PartialEq, Copy, Clone)]
+        enum State {
+            Valid,
             Invalid,
-            Unutilized,
         }
 
         fn valid(line: &Vec<u32>) -> bool {
             fn get_direction(a: u32, b: u32) -> Direction {
+                let diff = a.abs_diff(b);
                 match a < b {
-                    true => Direction::Up,
-                    false => Direction::Down,
+                    true => Direction::Up(diff),
+                    false => Direction::Down(diff),
                 }
             }
 
-            let (direction, _) = line.iter().skip(1).fold(
-                (Direction::Unutilized, line[0]),
-                |(direction, old), new| {
-                    let new_direction = get_direction(*new, old);
-                    let diff = new.abs_diff(old);
-
-                    match (
-                        direction,
-                        direction == new_direction,
-                        diff >= 1 && diff <= 3,
-                    ) {
-                        (Direction::Unutilized, _, true) => (new_direction, *new),
-                        (_, true, true) => (new_direction, *new),
-                        _ => return (Direction::Invalid, 0),
+            fn get_state(a: &Direction, b: &Direction) -> State {
+                match (a, b) {
+                    (Direction::Up(num1), Direction::Up(num2))
+                    | (Direction::Down(num1), Direction::Down(num2)) => {
+                        let diff = num1.abs_diff(*num2);
+                        match diff >= 1 && diff <= 3 {
+                            true => State::Valid,
+                            false => State::Invalid,
+                        }
                     }
-                },
-            );
+                    _ => State::Invalid,
+                }
+            }
 
-            !(Direction::Invalid == direction)
+            fn is_in_range(a: u32) -> bool {
+                a >= 1 && a <= 3
+            }
+
+            let directions = line
+                .iter()
+                .zip(line.iter().skip(1))
+                .map(|(a, b)| get_direction(*a, *b))
+                .collect::<Vec<_>>();
+
+            let result = directions
+                .iter()
+                .zip(directions.iter().skip(1))
+                .map(|(a, b)| match (a, b) {
+                    (Direction::Up(num1), Direction::Up(num2))
+                    | (Direction::Down(num1), Direction::Down(num2)) => {
+                        is_in_range(*num1) && is_in_range(*num2)
+                    }
+                    _ => false,
+                })
+                .map(|b| match b {
+                    true => State::Valid,
+                    false => State::Invalid,
+                })
+                .filter(|state| *state == State::Invalid)
+                .count()
+                == 0;
+            result
         }
 
         let lines = day2_parse("data/day02_part1.txt")?;
